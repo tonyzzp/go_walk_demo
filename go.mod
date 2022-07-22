dfd7cc75ffdf6e1f688d0b04fb90b891942bbd49
tonyzzp <tonyzzp@qq.com>
2022-07-22 10:50:48 +0800

    zenity

--- go.mod
+++ go.mod
@@ -4,8 +4,13 @@ go 1.18
 
 require (
 	github.com/akavel/rsrc v0.10.2 // indirect
+	github.com/dchest/jsmin v0.0.0-20220218165748-59f39799265f // indirect
+	github.com/josephspurrier/goversioninfo v1.4.0 // indirect
 	github.com/lxn/walk v0.0.0-20210112085537-c389da54e794 // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
+	github.com/ncruces/zenity v0.8.9 // indirect
+	github.com/randall77/makefat v0.0.0-20210315173500-7ddd0e42c844 // indirect
+	golang.org/x/image v0.0.0-20220617043117-41969df76e82 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,16 +3,28 @@ package main
 import (
 	"fmt"
 
+	"github.com/lxn/walk"
 	wui "github.com/lxn/walk/declarative"
+	"github.com/ncruces/zenity"
 )
 
 func main() {
+	var mainwin *walk.MainWindow
+	var mi_open *walk.Action
+	var s_status *walk.StatusBarItem
 	wui.MainWindow{
-		Name: "demo",
-		Size: wui.Size{
-			Width:  500,
-			Height: 300,
-		},
+		// Bounds: wui.Rectangle{
+		// 	X:      100,
+		// 	Y:      0,
+		// 	Width:  500,
+		// 	Height: 300,
+		// },
+		AssignTo: &mainwin,
+		Name:     "demo",
+		// Size: wui.Size{
+		// 	Width:  500,
+		// 	Height: 300,
+		// },
 		Title:  "demo",
 		Layout: wui.VBox{},
 		Children: []wui.Widget{
@@ -23,6 +35,54 @@ func main() {
 				Text: "btn",
 				OnClicked: func() {
 					fmt.Println("click")
+					var d, _ = walk.NewDialog(mainwin)
+					d.SetTitle("ss")
+					d.SetWidth(500)
+					d.SetHeight(300)
+					d.Show()
+				},
+			},
+		},
+		MenuItems: []wui.MenuItem{
+			wui.Menu{
+				Text: "file",
+				Items: []wui.MenuItem{
+					wui.Action{
+						Text:     "open",
+						AssignTo: &mi_open,
+						OnTriggered: func() {
+							fmt.Println("OnTriggered menu open")
+						},
+					},
+				},
+			},
+		},
+		StatusBarItems: []wui.StatusBarItem{
+			{
+				Text:     "status",
+				AssignTo: &s_status,
+				OnClicked: func() {
+					var result = walk.MsgBox(mainwin, "tips", "status ok", walk.MsgBoxIconInformation|walk.MsgBoxYesNo)
+					switch result {
+					case walk.DlgCmdYes:
+						fmt.Println("yes")
+					case walk.DlgCmdNo:
+						fmt.Println("ok")
+					}
+				},
+			},
+			{
+				Text: "aaa",
+				OnClicked: func() {
+					zenity.Info("click aaa", zenity.Title("tip"), zenity.InfoIcon)
+				},
+			},
+		},
+		ContextMenuItems: []wui.MenuItem{
+			wui.Action{
+				Text: "exit app",
+				OnTriggered: func() {
+					mainwin.Close()
 				},
 			},
 		},
